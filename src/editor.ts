5ef9d3d26e85f8da8d79921f455a033d66f436c1
charlie <jrwescom@msn.com>
2025-10-06 15:14:13 -0400

    Updated Config editor layout and switched  selectors

--- src/editor.ts
+++ src/editor.ts
@@ -7,18 +7,24 @@ export class AreaCardEliteEditor extends LitElement {
   @state() _config: AreaCardEliteConfig = { type: "area-card-elite", area: "" };
 
   private _schema = [
-    { name: "area", selector: { area: {} } },
-    { name: "name", selector: { text: {} } },
-    { name: "color", selector: { text: {} } },
-    { name: "display_type", selector: { select: { options: ["compact", "icon", "picture", "camera"] } } },
-    { name: "camera_view", selector: { select: { options: ["auto", "live"] } } },
-    { name: "aspect_ratio", selector: { text: {} } },
-    { name: "navigation_path", selector: { text: {} } },
-    { name: "alert_classes", selector: { object: {} } },
-    { name: "sensor_classes", selector: { object: {} } },
-    { name: "features", selector: { object: {} } },
-    { name: "features_position", selector: { select: { options: ["bottom", "inline"] } } },
-    { name: "exclude_entities", selector: { object: {} } }
+    { name: "area", selector: { area: {} }, label: "Area" },
+    { name: "name", selector: { text: {} }, label: "Custom Name" },
+    { type: "grid", name: "content", label: "Content", schema: [
+      { name: "display_type", selector: { select: { options: ["compact", "icon", "picture", "camera"] } }, label: "Display Type" },
+      { name: "color", selector: { color: {} }, label: "Color" },
+      { name: "aspect_ratio", selector: { text: {} }, label: "Aspect Ratio" },
+      { name: "camera_view", selector: { select: { options: ["auto", "live"] } }, label: "Camera View" }
+    ]},
+    { type: "grid", name: "interactions", label: "Interactions", schema: [
+      { name: "navigation_path", selector: { text: {} }, label: "Navigation Path" }
+    ]},
+    { type: "grid", name: "features", label: "Features", schema: [
+      { name: "alert_classes", selector: { select: { multiple: true, mode: "chips", options: ["moisture", "motion"] } }, label: "Alert Classes" },
+      { name: "sensor_classes", selector: { select: { multiple: true, mode: "chips", options: ["temperature", "humidity"] } }, label: "Sensor Classes" },
+      { name: "features", selector: { object: {} }, label: "Additional Features" },
+      { name: "features_position", selector: { select: { options: ["bottom", "inline"] } }, label: "Features Position" },
+      { name: "exclude_entities", selector: { entity: { multiple: true } }, label: "Exclude Entities" }
+    ]}
   ];
 
   setConfig(config: AreaCardEliteConfig): void {
